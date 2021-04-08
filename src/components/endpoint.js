21ff1969ab586ae68c59f7902c9aef28c897642d
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-04-08 15:48:11 +0530

    endpoints changed

--- src/components/endpoint.js
+++ src/components/endpoint.js
@@ -1,4 +1,6 @@
-const FRONTEND_ENDPOINT = 'http://localhost:3001';
-const BACKEND_ENDPOINT = 'http://localhost:3000';
+const FRONTEND_ENDPOINT = 'https://daily-dairy.netlify.app';
+const BACKEND_ENDPOINT = 'https://daily-dairy-server.herokuapp.com';
+// const FRONTEND_ENDPOINT = 'http://localhost:3001';
+// const BACKEND_ENDPOINT = 'http://localhost:3000';
 
 module.exports = { FRONTEND_ENDPOINT, BACKEND_ENDPOINT };
